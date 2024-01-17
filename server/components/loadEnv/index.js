ac014b1efe3b0436adbae485fecaf7d33b13d0db
Aman KUmar <amankumar.spj410@gmail.com>
2024-01-18 03:08:07 +0530

    added exception catch for env loader

--- server/components/loadEnv/index.js
+++ server/components/loadEnv/index.js
@@ -5,13 +5,17 @@ import * as fs from 'fs'
  * @param {string?} filePath Path to .env file (default: .env)
  */
 export default function loadEnv(filePath) {
-    const envPath = filePath || '.env'
-    const envFile = fs.readFileSync(envPath, 'utf-8')
-    const envLines = envFile.split('\n')
-
-    for (const line of envLines) {
-        const [key, value] = line.split('=')
-        process.env[key] = value
+    try {
+        const envPath = filePath || '.env'
+        const envFile = fs.readFileSync(envPath, 'utf-8')
+        const envLines = envFile.split('\n')
+    
+        for (const line of envLines) {
+            const [key, value] = line.split('=')
+            process.env[key] = value
+        }
+    } catch (error) {
+        console.error("File not found: " + filePath)
     }
 }
 
