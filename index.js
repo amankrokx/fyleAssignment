5613f2babe000d96b97147eb8f862c87ecabfb1d
Aman KUmar <amankumar.spj410@gmail.com>
2024-01-18 03:10:06 +0530

    added port from env

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ const reqHandler = requestHandler(routes)
 // Create the server
 const server = http.createServer(reqHandler)
 
+// Gather port from environment variables
+const port = process.env.PORT || 8080
+
 // Start the server
-server.listen(8080, () => {
+server.listen(port, () => {
     console.log('Server listening on port 8080')
     console.log('Visit http://localhost:8080 to see the magic happen!')
 })
